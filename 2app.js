3d8eeb36137677f0ae0e46314fab9ec2da7f5caa
Michelle <ogorzalek1@gmail.com>
2023-01-20 20:59:25 -0600

    Parallax test files, and fix, check toggle dark

--- 2app.js
+++ 2app.js
@@ -315,7 +315,7 @@ setInterval(rotateText, 2000);
 
 
 
-/* Parallax */
+/* Parallax
 let controller = new ScrollMagic.Controller();
 let timeline = new TimelineMax();
 
@@ -327,7 +327,7 @@ timeline
     .to('.covid1', 6, {y: 10}, '-=4')
     .to('.covid2', 6, {y: 10}, '-=4');
 
-/*this should be active per youtube video https://www.youtube.com/watch?v=Nt70Ld0dJCM
+this should be active per youtube video https://www.youtube.com/watch?v=Nt70Ld0dJCM
 but it prevents any animation.
 
 let scene = new ScrollMagic.Scene({
@@ -338,3 +338,27 @@ let scene = new ScrollMagic.Scene({
     .setTween(timeline)
     .setPin("nextContent")
     .addTo(controller) */
+
+/*Parallax*/
+let controller = new ScrollMagic.Controller();
+let timeline = new TimelineMax();
+
+timeline
+  .to(".covid1", 15, { y: -100 })  
+  .to(".covid2", 10, { y: -100 })
+  .to(".TB1", 15, { y: -100 })
+  .to(".TB2", 20, { y: -100 })
+  .to(".nurse", 50, { y: -95 }, "-=50")
+  .fromTo(".OR", { y: -50 }, { y: 0, duration: 10 }, "-=10")
+  .to(".content", 10, { top: "10%" }, "-=10")
+  .fromTo(".content-images", { opacity: 0 }, { opacity: 1, duration: 3 })
+  .fromTo(".text", { opacity: 0 }, { opacity: 1, duration: 3 });
+
+let scene = new ScrollMagic.Scene({
+  triggerElement: "section",
+  duration: "400%",
+  triggerHook: 0,
+})
+  .setTween(timeline)
+  .setPin("section")
+  .addTo(controller);
